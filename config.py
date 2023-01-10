b340ef8458f23ce12dd04aa1df84ac00ba01d10b
cool3477 <65460437+cool3477@users.noreply.github.com>
2023-01-10 14:48:38 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-USER_AGENT = "Mozilla/5.0 (Linux; Android 10; SM-G980F Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.96 Mobile Safari/537.36"
+USER_AGENT = "hello"
